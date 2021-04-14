d108e5a2b345163574e5bb952b20e1bc2f867da0
autopp <autopp.inc@gmail.com>
2021-04-15 02:58:22 +0900

    initial commit

---
+++ src/pages/_app.tsx
@@ -0,0 +1,7 @@
+import "../styles/global.css"
+import { AppProps } from "next/app"
+import { ReactElement } from "react"
+
+export default function App({ Component, pageProps }: AppProps): ReactElement {
+  return <Component {...pageProps} />
+}
